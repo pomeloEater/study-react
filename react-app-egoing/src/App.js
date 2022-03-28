0a50a080fb40de35a37ee0879e18e10d3dfd1396
jinhui.dev <jinhui.dev@gmail.com>
2022-03-28 15:10:52 +0900

    [update] header, nav, article 구조로 수정

--- react-app-egoing/src/App.js
+++ react-app-egoing/src/App.js
@@ -1,23 +1,108 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+
+function Header(props) {
+  console.log('props',props,props.title);
+  return (
+    <header>
+      <h1><a href="/" onClick={(event) => {
+        event.preventDefault(); // a 태그 기본 동작 이벤트(reload) 방지
+        props.onChangeMode();
+      }}>{props.title}</a></h1>
+    </header>
+  );
+}
+
+function Nav(props) {
+ /*
+ // Array.map 함수 이용하여 jsx array 생성하기
+  const topics = props.topics;
+  const listItems = topics.map((topic) => 
+      <li key={topic.id}>
+        <a id={topic.id} href={'/read/' + topic.id} onClick={(event) => {
+          event.preventDefault();
+          props.onChangeMode(Number(event.target.id));
+        }}>{topic.title}</a>
+      </li>
+    );
+  // return 안에서서 topics.map((topic) => {}) 은 안 되는 이유는?
+  return (
+    <nav>
+      <ol>
+        {listItems}
+      </ol>
+    </nav>
+  );
+  */
+  const lis = [];
+  for (let i = 0; i < props.topics.length; i++) {
+    let t = props.topics[i];
+    lis.push(<li key={t.id}>
+      <a id={t.id} href={'/read/' + t.id} onClick={event => {
+        event.preventDefault();
+        props.onChangeMode(Number(event.target.id));
+      }}>{t.title}</a></li>);
+  }
+  return (
+    <nav>
+      <ol>
+        {lis}
+      </ol>
+    </nav>
+  );
+}
+
+function Article(props) {
+  return (
+    <article>
+      <h2>{props.title}</h2>
+      {props.body}
+    </article>
+  );
+}
+
 
 function App() {
+  // const _mode = useState('WELCOME');
+  // const mode = _mode[0];
+  // const setMode = _mode[1];
+  // console.log('_mode', _mode);
+  const [mode, setMode] = useState('WELCOME');
+  const [id, setId] = useState(null);
+  const topics = [
+    {id:1, title:'html', body:'html is ...'},
+    {id:2, title:'css', body:'css is ...'},
+    {id:3, title:'javascript', body:'javascript is ...'},
+  ];
+  let content = null;
+
+  if (mode === 'WELCOME') {
+    content = <Article title="Welcome" body="Hello, WEB"></Article>
+  } else if (mode === 'READ') {
+    let title, body = null;
+    for (let i = 0; i < topics.length; i++) {
+      // props.onChangeMode(event.target.id) 와 props.onChangeMode(Number(event.target.id)) 의 차이점
+      // 
+      console.log(topics[i].id, id);
+      if (topics[i].id === id) {
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+    content = <Article title={title} body={body}></Article>
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header title="REACT" onChangeMode={() => {
+        setMode('WELCOME');
+      }}></Header>
+      <Nav topics={topics} onChangeMode={(_id) => {
+        setMode('READ');
+        setId(_id);
+      }}></Nav>
+      {content}
     </div>
   );
 }
