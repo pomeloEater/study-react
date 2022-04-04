7045d08c3b527d5a00ab271b70d3aa9b1557944f
jinhui.dev <jinhui.dev@gmail.com>
2022-04-04 17:51:58 +0900

    [update/LRV-17] 리덕스 연동(connect) 컴포넌트 만들기

---
+++ learning-react-velopert/17-react-redux/src/containers/CounterContainer.js
@@ -0,0 +1,81 @@
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import Counter from '../components/Counter';
+import { increase, decrease } from '../modules/counter';
+
+const CounterContainer = ({ number, increase, decrease }) => {
+  return (
+    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+  );
+};
+/*
+  connect(mapStateToProps, mapDispatchToProps)(연동할 컴포넌트)
+    - mapStateToProps
+      : 리덕스 스토어 안의 상태를 컴포넌트의 props로 넘겨주기 위해 설정하는 함수
+    - mapDispatchToProps
+      : 액션 생성 함수를 컴포넌트의 props로 넘겨주기 위해 사용하는 함수
+      store의 내장 함수 dispatch를 파라미터로 받아온다
+    => connect 함수 호출하면 다른 함수를 반환
+      => 반환된 함수에 컴포넌트를 파라미터로 넣어주면 리덕스와 연동된 컴포넌트가 만들어진다
+ */
+
+// const mapStateToProps = (state) => ({
+//   number: state.counter.number,
+// });
+
+// const mapDispatchToProps = (dispatch) => ({
+//   // 임시 함수
+//   increase: () => {
+//     // console.log('increase');
+//     dispatch(increase());
+//   },
+//   decrease: () => {
+//     // console.log('decrease');
+//     dispatch(decrease());
+//   },
+// });
+
+// export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
+
+/* 
+  // 익명 함수를 사용하면 더욱 간단하게 표현될 때도 있다 
+  export default connect(
+    state => ({
+      number: state.counter.number,
+    }),
+    dispatch => ({
+      increase: () => dispatch(increase()),
+      // increase: () => { return dispatch(increase()); } 와 동일
+      decrease: () => dispatch(decrease()),
+    }),
+  )(CounterContainer);
+
+*/
+
+/* bindActionCreators 유틸 함수를 사용 */
+export default connect(
+  (state) => ({
+    number: state.counter.number,
+  }),
+  (dispatch) =>
+    bindActionCreators(
+      {
+        increase,
+        decrease,
+      },
+      dispatch,
+    ),
+)(CounterContainer);
+
+/* 
+  두 번째 파라미터를 아예 객체 형태로 넣어 주면 connect 함수가 내부적으로 bindActionCreators 작업을 대신해 준다
+  export default connect(
+    state => ({
+      number: state.counter.number,
+    }),
+    {
+      increase,
+      decrease,
+    },
+  )(CounterContainer);
+*/
