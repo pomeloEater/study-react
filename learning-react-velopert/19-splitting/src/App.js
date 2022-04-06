19e2f47901e084beaa54de7d1285c3dedcc5cffa
jinhui.dev <jinhui.dev@gmail.com>
2022-04-06 15:01:05 +0900

    [update/LRV-19] React.lazy()와 Suspense - 개발자도구 Network에서 Slow 3G 설정한 후 Hello React! 클릭 -> 로딩 문구 못봄...

--- learning-react-velopert/19-splitting/src/App.js
+++ learning-react-velopert/19-splitting/src/App.js
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { Suspense, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 // import notify from "./notify";
+const SplitMe = React.lazy(() => import("./SplitMe"));
 
 function App() {
+  const [visible, setVisible] = useState(false);
   const onClick = () => {
     // notify();
-    import("./notify").then((result) => result.default());
+    // import("./notify").then((result) => result.default());
+    setVisible(true);
   };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p onClick={onClick}>Hello React!</p>
+        <Suspense fallback={<div>loading...</div>}>
+          {visible && <SplitMe />}
+        </Suspense>
       </header>
     </div>
   );
