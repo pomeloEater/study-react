88ff077d9b451f618ad600af750c27380bf83eac
jinhui.dev <jinhui.dev@gmail.com>
2022-04-06 10:37:55 +0900

    [update/LRV-18] redux-thunk로 비동기 작업 처리하기 - Uncaught Error:  Actions must be plain objects.   Instead, the actual type was: 'Promise'.   액션 실행 함수에서 발생하는 오류 (혹, dispatch가 빠지지 않았는지 확인)   😂Container에서 웹 요청 비동기 함수를 호출할 때 알맞는 함수를 import 하고 있는지 확인해볼 필요가 있다

--- learning-react-velopert/18-middleware/src/index.js
+++ learning-react-velopert/18-middleware/src/index.js
@@ -6,10 +6,11 @@ import App from "./App";
 import rootReducer from "./modules";
 // import loggerMiddleware from "./lib/loggerMiddleware";
 import { createLogger } from "redux-logger";
+import ReduxThunk from "redux-thunk";
 
 // const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
 const logger = createLogger();
-const store = createStore(rootReducer, applyMiddleware(logger));
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk, logger));
 
 const container = document.getElementById("root");
 const root = createRoot(container);
